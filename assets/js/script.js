7ea77956f616103bc97ba7e452def4d6cd213050
ashismondal18 <143177996+ashismondal18@users.noreply.github.com>
2024-05-06 08:23:20 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
+"use strict";
+window.addEventListener("DOMContentLoaded", () => {
+  const btns = document.querySelectorAll("button"),
+    links = document.querySelectorAll("a");
+
+  function PreventerDefault(value) {
+    value.forEach((item) => {
+      item.addEventListener("click", (e) => {
+        e.preventDefault();
+        console.log("Test 1 Done 👍");
+      });
+    });
+  }
+  PreventerDefault(btns);
+  PreventerDefault(links);
+});
 
